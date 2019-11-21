1d3aafc969bcfaa6a8a321b89f538b5093ece719
vic <vic@NIKIFOROS>
2019-11-21 23:13:19 +0200

    improvement on logging

--- nnmf-vicangel/src/main/java/helpful_classes/AppLogger.java
+++ nnmf-vicangel/src/main/java/helpful_classes/AppLogger.java
@@ -26,7 +26,10 @@ public class AppLogger {
 
 			// This block configure the logger with handler and formatter
 			FileHandler fh = new FileHandler(Constants.loggerPath + fileToWrite);
+			// In project.log will be logged everything on the project
+			FileHandler fh1 = new FileHandler(Constants.loggerPath + "projectLog.log");
 			logger.addHandler(fh);
+			logger.addHandler(fh1);
 			SimpleFormatter formatter = new SimpleFormatter();
 			fh.setFormatter(formatter);
 			// prevent to write to console output

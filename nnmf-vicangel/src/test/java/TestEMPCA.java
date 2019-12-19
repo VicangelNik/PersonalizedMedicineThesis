1d4836e555a1d4305268b7c5d3df184b640b597e
vic <vic@NIKIFOROS>
2019-12-19 02:29:31 +0200

    improve on empca test

--- nnmf-vicangel/src/test/java/TestEMPCA.java
+++ nnmf-vicangel/src/test/java/TestEMPCA.java
@@ -5,12 +5,14 @@ import java.util.Enumeration;
 import java.util.List;
 
 import org.junit.Test;
+import org.scify.EMPCA.EMPCA;
 
 import helpful_classes.Constants;
 import scala.Tuple2;
 import weka.api.library.LoadArff;
 import weka.core.Attribute;
 import weka.core.Instance;
+import weka.core.Instances;
 
 /*
  * The standard Scala backend is a Java VM. Scala classes are Java classes, and vice versa. 
@@ -26,27 +28,28 @@ public class TestEMPCA {
 		File level2File = new File(Constants.SRC_MAIN_RESOURCES_PATH + "PatientAndCïntrolProcessedLevelTwo.arff");
 		LoadArff arffLoader = new LoadArff(level2File);
 		arffLoader.setClassIndex(arffLoader.getStructure().attribute("SampleStatus").index());
-		Instance current;
-		int count = 0;
-
-		// ArrayList<List<Tuple2<Integer,Double>>>[] genList = new
-		// ArrayList[arffLoader.getDataSet().numInstances()];
-		List<Tuple2<Integer, Double>>[] list = new ArrayList[arffLoader.getDataSet().numInstances()
-				* arffLoader.getDataSet().numAttributes()];
-		while ((current = arffLoader.getNextInstance(arffLoader.getStructure())) != null) {
-
-			Enumeration<Attribute> enumeration = current.enumerateAttributes();
-			int count1 = 0;
-			while (enumeration.hasMoreElements()) {
-				Attribute attribute = enumeration.nextElement();
+		Instances data = arffLoader.getDataSet();
+		@SuppressWarnings("unchecked")
+		List<Tuple2<Integer, Double>>[] javaList = new ArrayList[data.numInstances()];
+		for (int i = 0; i < data.numInstances(); i++) { 
+			javaList[i] = new ArrayList<Tuple2<Integer, Double>>(); 
+        } 
+		int insCount = 0;
+		Enumeration<Instance> instEnumeration = data.enumerateInstances();
+		Enumeration<Attribute> attEnumeration = data.enumerateAttributes();
+		while (instEnumeration.hasMoreElements()) {
+			Instance current = instEnumeration.nextElement();
+			while (attEnumeration.hasMoreElements()) {
+				Attribute attribute = attEnumeration.nextElement();
 				int attIndex = attribute.index();
-				list[count] = new ArrayList<>();
-				list[count1].add(new Tuple2<Integer, Double>(attIndex, current.value(attIndex)));
+				javaList[insCount].add(new Tuple2<Integer, Double>(attIndex, current.value(attIndex)));
 			}
-			count++;
+			insCount++;
 		}
-		;
-		// JavaConversions.asScalaSet(list).toList();
-		// EMPCA empca = new EMPCA(list, 50);
+		@SuppressWarnings("unchecked")
+		scala.collection.immutable.List<Tuple2<Object, Object>>[] scalaList = (scala.collection.immutable.List<Tuple2<Object, Object>>[]) javaList;
+
+		EMPCA empca = new EMPCA(scalaList, 50);
 	}
+
 }

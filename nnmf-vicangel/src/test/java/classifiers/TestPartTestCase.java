3eb2d3fdff52b9c0c84cf48afaa6a4523d0fce4e
VicangelNik <cs121031@teiath.gr>
2020-05-25 00:04:44 +0300

    IBK PART and JRIP tests and results

---
+++ nnmf-vicangel/src/test/java/classifiers/TestPartTestCase.java
@@ -0,0 +1,78 @@
+package classifiers;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import helpful_classes.Constants;
+import utilpackage.WekaUtils;
+import weka.classifiers.AbstractClassifier;
+import weka.core.Instances;
+
+/**
+ * The Class TestPartTestCase.
+ */
+public class TestPartTestCase {
+
+	/** The dataset file name. */
+	// private final String datasetFileName = Constants.WEKA_FILES + "iris.arff";
+	private final String completeFileName = Constants.SRC_MAIN_RESOURCES_PATH
+			+ "PatientAndControlProcessedLevelTwo.arff";
+
+	/** The meth file name. */
+	private final String methFileName = Constants.SRC_MAIN_RESOURCES_PATH + "methDataset.arff";
+
+	/** The mi RNA file name. */
+	private final String miRNAFileName = Constants.SRC_MAIN_RESOURCES_PATH + "miRNADataset.arff";
+
+	/** The m RNA file name. */
+	private final String mRNAFileName = Constants.SRC_MAIN_RESOURCES_PATH + "mRNADataset.arff";
+
+	/** The num folds. */
+	private int numFolds = 10;
+
+	/** The random. */
+	private int random = 1;
+
+	/** The class name. */
+	// private final String className = "class";
+	private final String className = "SampleStatus";
+
+	/** The file names. */
+	private final String[] fileNames = new String[] { completeFileName, methFileName, miRNAFileName, mRNAFileName };
+
+	/**
+	 * Inits the.
+	 *
+	 * @param testInfo the test info
+	 */
+	@BeforeEach
+	void init(TestInfo testInfo) {
+		System.out.println(testInfo.getDisplayName());
+	}
+
+	/**
+	 * Test part default.
+	 */
+	@Test
+	@DisplayName("Part Default")
+	public void testPartDefault() {
+		for (String datasetFileName : fileNames) {
+			System.out.println(datasetFileName.toUpperCase());
+			try {
+				File level2File = new File(datasetFileName);
+				Instances originalDataset = WekaUtils.getOriginalData(level2File, className);
+				AbstractClassifier classifier = WekaUtils.getClassifier(Constants.PART, originalDataset,
+						new String[] {});
+				WekaUtils.crossValidationAction(Constants.PART, classifier, numFolds, random);
+			} catch (IOException e) {
+				Assert.assertFalse(e.getMessage(), true);
+			}
+		}
+	}
+}

4e7fbba35039063db51a33d512754ce7a30d59d1
VicangelNik <cs121031@teiath.gr>
2020-06-28 23:12:16 +0300

    junit core with listener for sound notification

---
+++ nnmf-vicangel/src/test/java/junit/core/main/TestListenerTestCase.java
@@ -0,0 +1,18 @@
+package junit.core.main;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The Class TestListenerTestCase.
+ */
+public class TestListenerTestCase {
+
+	/**
+	 * Test.
+	 */
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+	}
+}

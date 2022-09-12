479b66232a8d25d887b0719cb446f46b0b0333fa
nxylo <nikiforos.xylogiannopoulos@ibm.com>
2022-09-12 10:15:20 +0300

    parameterized tests in nonNegativeMatrixFactorizationTests

--- src/test/java/personalizedmedicine/TestNonNegativeMatrixFactorizationRegressionTestCase.java
+++ src/test/java/personalizedmedicine/TestNonNegativeMatrixFactorizationRegressionTestCase.java
@@ -11,35 +11,40 @@ import org.jblas.DoubleMatrix;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import personalizedmedicine.dimensionality_reduction_methods.NonNenagativeMatrixFactorization;
-import personalizedmedicine.helpful_classes.Constants;
 import personalizedmedicine.utilpackage.Utils;
 
+import static personalizedmedicine.helpful_classes.Constants.SRC_TEST_RESOURCES_PATH;
+import static personalizedmedicine.utilpackage.Utils.FILE_DATA_PATH;
+import static personalizedmedicine.utilpackage.Utils.TXT_SUFFIX;
+
 class TestNonNegativeMatrixFactorizationRegressionTestCase {
-    private static double[][] doubleArray;
     private static DoubleMatrix matrixX;
     private static DoubleMatrix matrixW;
     private static DoubleMatrix matrixH;
-    private static int num_components = 2;
-    private static double tolerance = 1e-6;
-    private static int maxIterations = 10000;
+    private static final int num_components = 2;
+    private static final double tolerance = 1e-6;
+    private static final int maxIterations = 10000;
+    private TestInfo testInfo;
 
-	@BeforeEach
-	public void initializeMatrix() {
-		// initialize array
-		doubleArray = new double[400][4];
-		// set numerical parameters to the array
-		Utils.setParametersToArray(doubleArray);
-		// initialize matrixX
-		matrixX = new DoubleMatrix(doubleArray);
-		// write data with the class attribute into file
-		Utils.writeMatrixToFile(Utils.FILE_DATA_PATH, matrixX);
-	}
+    @BeforeEach
+    public void initializeMatrix(final TestInfo testInfo) {
+        this.testInfo = testInfo;
+        // initialize array
+        final double[][] doubleArray = new double[400][4];
+        // set numerical parameters to the array
+        Utils.setParametersToArray(doubleArray);
+        // initialize matrixX
+        matrixX = new DoubleMatrix(doubleArray);
+        // write data with the class attribute into file
+        Utils.writeMatrixToFile(FILE_DATA_PATH, matrixX);
+    }
 
     @AfterAll
     public static void clearResources() {
         // remove all the files of the directory but not the files in subdirectories
-        Utils.removeFiles(Constants.SRC_TEST_RESOURCES_PATH);
+        Utils.removeFiles(SRC_TEST_RESOURCES_PATH);
     }
 
     @Test
@@ -63,13 +68,11 @@ class TestNonNegativeMatrixFactorizationRegressionTestCase {
         // add to matrices the regression matrix H
         matrices[3] = matrixRegressionH;
         // write results to files
-        Utils.writeResults(new Object() {}.getClass().getEnclosingMethod().getName(), Utils.TXT_SUFFIX, matrices,
-                           measureResult);
+        Utils.writeResults(testInfo.getDisplayName(), TXT_SUFFIX, matrices, measureResult);
         // Calculate new measure after regression
         measureResult = measure.get(matrixX, matrixW, matrixRegressionH);
         // write new measure after regression
-        Utils.writeResults(new Object() {}.getClass().getEnclosingMethod().getName(), Utils.TXT_SUFFIX, null,
-                           measureResult);
+        Utils.writeResults(testInfo.getDisplayName(), TXT_SUFFIX, null, measureResult);
     }
 
     @Test
@@ -93,13 +96,11 @@ class TestNonNegativeMatrixFactorizationRegressionTestCase {
         // add to matrices the regression matrix H
         matrices[3] = matrixRegressionH;
         // write results to files
-        Utils.writeResults(new Object() {}.getClass().getEnclosingMethod().getName(), Utils.TXT_SUFFIX, matrices,
-                           measureResult);
+        Utils.writeResults(testInfo.getDisplayName(), TXT_SUFFIX, matrices, measureResult);
         // Calculate new measure after regression
         measureResult = measure.get(matrixX, matrixW, matrixRegressionH);
         // write new measure after regression
-        Utils.writeResults(new Object() {}.getClass().getEnclosingMethod().getName(), Utils.TXT_SUFFIX, null,
-                           measureResult);
+        Utils.writeResults(testInfo.getDisplayName(), TXT_SUFFIX, null, measureResult);
     }
 
     @Test
@@ -123,13 +124,11 @@ class TestNonNegativeMatrixFactorizationRegressionTestCase {
         // add to matrices the regression matrix H
         matrices[3] = matrixRegressionH;
         // write results to files
-        Utils.writeResults(new Object() {}.getClass().getEnclosingMethod().getName(), Utils.TXT_SUFFIX, matrices,
-                           measureResult);
+        Utils.writeResults(testInfo.getDisplayName(), TXT_SUFFIX, matrices, measureResult);
         // Calculate new measure after regression
         measureResult = measure.get(matrixX, matrixW, matrixRegressionH);
         // write new measure after regression
-        Utils.writeResults(new Object() {}.getClass().getEnclosingMethod().getName(), Utils.TXT_SUFFIX, null,
-                           measureResult);
+        Utils.writeResults(testInfo.getDisplayName(), TXT_SUFFIX, null, measureResult);
     }
 
     @Test
@@ -153,12 +152,10 @@ class TestNonNegativeMatrixFactorizationRegressionTestCase {
         // add to matrices the regression matrix H
         matrices[3] = matrixRegressionH;
         // write results to files
-        Utils.writeResults(new Object() {}.getClass().getEnclosingMethod().getName(), Utils.TXT_SUFFIX, matrices,
-                           measureResult);
+        Utils.writeResults(testInfo.getDisplayName(), TXT_SUFFIX, matrices, measureResult);
         // Calculate new measure after regression
         measureResult = measure.get(matrixX, matrixW, matrixRegressionH);
         // write new measure after regression
-        Utils.writeResults(new Object() {}.getClass().getEnclosingMethod().getName(), Utils.TXT_SUFFIX, null,
-                           measureResult);
+        Utils.writeResults(testInfo.getDisplayName(), TXT_SUFFIX, null, measureResult);
     }
 }
--- src/test/java/personalizedmedicine/TestNonNegativeMatrixFactorizationTestCase.java
+++ src/test/java/personalizedmedicine/TestNonNegativeMatrixFactorizationTestCase.java
@@ -4,106 +4,76 @@ import lombok.val;
 import org.dulab.javanmf.measures.EuclideanDistance;
 import org.dulab.javanmf.measures.KLDivergence;
 import org.dulab.javanmf.measures.Measure;
-import org.dulab.javanmf.updaterules.*;
+import org.dulab.javanmf.updaterules.FGDMUpdateRule;
+import org.dulab.javanmf.updaterules.MKLUpdateRule;
+import org.dulab.javanmf.updaterules.MUpdateRule;
+import org.dulab.javanmf.updaterules.RegularizationUpdateRule;
 import org.jblas.DoubleMatrix;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import personalizedmedicine.dimensionality_reduction_methods.NonNenagativeMatrixFactorization;
-import personalizedmedicine.helpful_classes.Constants;
 import personalizedmedicine.utilpackage.Utils;
 
+import java.util.stream.Stream;
+
+import static personalizedmedicine.helpful_classes.Constants.SRC_TEST_RESOURCES_PATH;
+import static personalizedmedicine.utilpackage.Utils.FILE_DATA_PATH;
+import static personalizedmedicine.utilpackage.Utils.TXT_SUFFIX;
+
 public class TestNonNegativeMatrixFactorizationTestCase {
-    private static double doubleArray[][];
     private static DoubleMatrix matrixX;
-    private static DoubleMatrix matrixW;
-    private static DoubleMatrix matrixH;
-    private static int num_components = 2;
+    private static final int num_components = 2;
+    private TestInfo testInfo;
 
     @BeforeEach
     public void initializeMatrix() {
         // initialize array
-        doubleArray = new double[400][4];
+        final double[][] doubleArray = new double[400][4];
         // set numerical parameters to the array
         Utils.setParametersToArray(doubleArray);
         // initialize matrixX
         matrixX = new DoubleMatrix(doubleArray);
         // write data with the class attribute into file
-        Utils.writeMatrixToFile(Utils.FILE_DATA_PATH, matrixX);
+        Utils.writeMatrixToFile(FILE_DATA_PATH, matrixX);
     }
 
     @AfterAll
     public static void clearResources() {
         // remove all the files of the directory but not the files in subdirectories
-        Utils.removeFiles(Constants.SRC_TEST_RESOURCES_PATH);
+        Utils.removeFiles(SRC_TEST_RESOURCES_PATH);
     }
 
-    @Test
-    void testEuclideanDistanceWithFGDMUpdateRuleTestCase() {
+    @ParameterizedTest
+    @MethodSource
+    void testEuclideanDistanceWithFGDMUpdateRuleTestCase(final RegularizationUpdateRule rule, final Measure measure) {
         // Create matrices W and H
-        matrixW = new DoubleMatrix(matrixX.rows, num_components);
-        matrixH = new DoubleMatrix(num_components, matrixX.columns);
+        val matrixW = new DoubleMatrix(matrixX.rows, num_components);
+        val matrixH = new DoubleMatrix(num_components, matrixX.columns);
         // do factorization
         val nnmf = new NonNenagativeMatrixFactorization(matrixW, matrixH, matrixX);
-        val rule = new FGDMUpdateRule(1.0, 1.0);
         val matrices = nnmf.nonNegativeMatrixFactorizationImp(rule);
-        // Calculates the distance between matrices X and WH using the euclidean
-        // distance || X - WH || ^2
-        val measure = new EuclideanDistance();
+
         final double measureResult = measure.get(matrixX, matrixW, matrixH);
         // write results to files
-        Utils.writeResults(new Object() {}.getClass().getEnclosingMethod().getName(), Utils.TXT_SUFFIX, matrices,
-                           measureResult);
+        Utils.writeResults(testInfo.getDisplayName(), TXT_SUFFIX, matrices, measureResult);
     }
 
-    @Test
-    void testEuclideanDistanceWithMUpdateRuleTestCase() {
-        // Create matrices W and H
-        matrixW = new DoubleMatrix(matrixX.rows, num_components);
-        matrixH = new DoubleMatrix(num_components, matrixX.columns);
-        val nnmf = new NonNenagativeMatrixFactorization(matrixW, matrixH, matrixX);
-        val rule = new MUpdateRule(1.0, 1.0);
-        Object[] matrices = nnmf.nonNegativeMatrixFactorizationImp(rule);
-        // Calculates the distance between matrices X and WH using the euclidean
-        // distance || X - WH || ^2
-        val measure = new EuclideanDistance();
-        final double measureResult = measure.get(matrixX, matrixW, matrixH);
-        // write results to files
-        Utils.writeResults(new Object() {}.getClass().getEnclosingMethod().getName(), Utils.TXT_SUFFIX, matrices,
-                           measureResult);
-    }
+    private static Stream<Arguments> testEuclideanDistanceWithFGDMUpdateRuleTestCase() {
+        val fgmdUpdateRule = new FGDMUpdateRule(1.0, 1.0);
+        val mUpdateRule = new MUpdateRule(1.0, 1.0);
+        val mklUpdateRule = new MKLUpdateRule(1.0, 1.0);
+        // Calculates the distance between matrices X and WH using the euclidean distance || X - WH || ^2
+        val euclideanDistanceMeasure = new EuclideanDistance();
+        // Calculates the distance between matrices X and WH using the generalized Kullback-Leibler divergence Σ ( X log ( X / WH ) - X + WH )
+        val klDivergenceMeasure = new KLDivergence();
 
-    @Test
-    void testKullbackLeiblerWithFGDKUpdateRuleTestCase() {
-        // Create matrices W and H
-        matrixW = new DoubleMatrix(matrixX.rows, num_components);
-        matrixH = new DoubleMatrix(num_components, matrixX.columns);
-        NonNenagativeMatrixFactorization nnmf = new NonNenagativeMatrixFactorization(matrixW, matrixH, matrixX);
-        UpdateRule rule = new FGDKLUpdateRule(1.0, 1.0);
-        Object[] matrices = nnmf.nonNegativeMatrixFactorizationImp(rule);
-        // Calculates the distance between matrices X and WH using the generalized
-        // Kullback-Leibler divergence Σ ( X log ( X / WH ) - X + WH )
-        Measure measure = new KLDivergence();
-        double measureResult = measure.get(matrixX, matrixW, matrixH);
-        // write results to files
-        Utils.writeResults(new Object() {}.getClass().getEnclosingMethod().getName(), Utils.TXT_SUFFIX, matrices,
-                           measureResult);
-    }
-
-    @Test
-    void testKullbackLeiblerWithMKLUpdateRuleTestCase() {
-        // Create matrices W and H
-        matrixW = new DoubleMatrix(matrixX.rows, num_components);
-        matrixH = new DoubleMatrix(num_components, matrixX.columns);
-        val nnmf = new NonNenagativeMatrixFactorization(matrixW, matrixH, matrixX);
-        val rule = new MKLUpdateRule(1.0, 1.0);
-        val matrices = nnmf.nonNegativeMatrixFactorizationImp(rule);
-        // Calculates the distance between matrices X and WH using the generalized
-        // Kullback-Leibler divergence Σ ( X log ( X / WH ) - X + WH )
-        val measure = new KLDivergence();
-        final double measureResult = measure.get(matrixX, matrixW, matrixH);
-        // write results to files
-        Utils.writeResults(new Object() {}.getClass().getEnclosingMethod().getName(), Utils.TXT_SUFFIX, matrices,
-                           measureResult);
+        return Stream.of(Arguments.of(fgmdUpdateRule, euclideanDistanceMeasure),
+                         Arguments.of(mUpdateRule, euclideanDistanceMeasure),
+                         Arguments.of(fgmdUpdateRule, klDivergenceMeasure),
+                         Arguments.of(mklUpdateRule, klDivergenceMeasure));
     }
 }
